74bf86b2a327f05911930e5d009e6c28abef26c6
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-28 23:58:05 +0530

    ...

---
+++ School/src/components/ActivitiesSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ActivitiesSection = () => {
+    return (
+        <section id="activities" className="p-8 bg-white">
+            <h2 className="text-3xl text-center">School Activities</h2>
+            <ul className="mt-4 text-center">
+                <li>Sports Day</li>
+                <li>Art and Craft Exhibition</li>
+                <li>Science Fair</li>
+                <li>Annual Day Celebration</li>
+            </ul>
+        </section>
+    );
+};
+
+export default ActivitiesSection;

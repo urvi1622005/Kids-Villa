56493f9416330f45d757e2f5001ad0ff5a2cbe82
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-02-04 23:41:34 +0530

    ...

---
+++ retextify/controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// User signup
+exports.signup = async (req, res) => {
+    // Implementation here
+};
+
+// User login
+exports.login = async (req, res) => {
+    // Implementation here
+};

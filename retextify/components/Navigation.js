3286b60bf42ce5bcf5c651df3ce0e1e8c54d2d72
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-02-17 23:51:42 +0530

    ..

--- retextify/components/Navigation.js
+++ retextify/components/Navigation.js
@@ -3,6 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import DocumentScanner from './DocumentScanner'; // Import Document Scanner Screen
 import OCRScreen from './OCRScreen'; // Import OCR Screen
+import LoginScreen from './LoginScreen';
+import SignUpScreen from './SignupScreen';
+import SplashScreen from './SplashScreen';
+
 
 const Stack = createStackNavigator();
 
@@ -14,6 +18,9 @@ const AppNavigator = () => {
         {/* <Stack.Screen name="DocumentScanner" component={DocumentScanner} /> */}
 
         {/* OCR Screen */}
+        {/* <Stack.Screen name="loginScreen" component={LoginScreen} /> */}
+        {/* <Stack.Screen name="SignUpScreen" component={SignUpScreen} />   */}
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="OCRScreen" component={OCRScreen} />
         <Stack.Screen name="DocumentScanner" component={DocumentScanner}/>
       </Stack.Navigator>

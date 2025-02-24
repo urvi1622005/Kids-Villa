f26df2b6ddc48755acc736d3e0c30568b501f3cc
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-02-24 23:24:13 +0530

    ..

--- retextify/App.js
+++ retextify/App.js
@@ -13,63 +13,63 @@ const Stack = createStackNavigator();
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [userToken, setUserToken] = useState(null);
-```javascript
-import React, { useState, useEffect } from 'react';
-import { ActivityIndicator } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './components/LoginScreen';
-import SignupScreen from './components/SignupScreen';
-import ForgotPassword from './components/ForgotPassword';
-import Home from './components/Home';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// ```javascript
+// import React, { useState, useEffect } from 'react';
+// import { ActivityIndicator } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import LoginScreen from './components/LoginScreen';
+// import SignupScreen from './components/SignupScreen';
+// import ForgotPassword from './components/ForgotPassword';
+// import Home from './components/Home';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [userToken, setUserToken] = useState(null);
+// const App = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [userToken, setUserToken] = useState(null);
 
-  const checkLoginStatus = async () => {
-    try {
-      const token = await AsyncStorage.getItem('userToken');
-      setUserToken(token);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+//   const checkLoginStatus = async () => {
+//     try {
+//       const token = await AsyncStorage.getItem('userToken');
+//       setUserToken(token);
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
 
-  useEffect(() => {
-    checkLoginStatus();
-  }, []);
+//   useEffect(() => {
+//     checkLoginStatus();
+//   }, []);
 
-  if (isLoading) {
-    return <ActivityIndicator size="large" color="#FF5722" />;
-  }
+//   if (isLoading) {
+//     return <ActivityIndicator size="large" color="#FF5722" />;
+//   }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {userToken ? (
-          <>
-            <Stack.Screen name="Home" component={Home} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Signup" component={SignupScreen} />
-            <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         {userToken ? (
+//           <>
+//             <Stack.Screen name="Home" component={Home} />
+//           </>
+//         ) : (
+//           <>
+//             <Stack.Screen name="Login" component={LoginScreen} />
+//             <Stack.Screen name="Signup" component={SignupScreen} />
+//             <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
-export default App;
-```
+// export default App;
+// ```
   const checkLoginStatus = async () => {
     try {
       const token = await AsyncStorage.getItem('userToken');
